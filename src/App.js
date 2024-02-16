6e4a946aa5f8b34e48da888c82be740deea13a52
Henok Derbew <derbew7694@gmail.com>
2024-02-16 18:02:19 -0500

    4th Commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Weather from './weather';
 
 
+
 const App = () => {
 return (
     <div>
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  /* @__PURE__ */ jsxDEV(React.StrictMode, { children: /* @__PURE__ */ jsxDEV(App, {}, void 0, false, {
+    // ...
+  })}, void 0, false, {
+    // ...
+  })
+);
