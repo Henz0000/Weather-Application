86c942fb5c9cb0d02b60644af22ceb981b6cd738
Henok Derbew <derbew7694@gmail.com>
2024-02-16 20:13:10 -0500

    Functioning Weather App

---
+++ src/Weather.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "React";
+import axois from "axios";
+
+const Weather = () => {
+  const [city, setCity] = useState("London");
+  const [weatherData, setweatherData] = useState(null);
+  const fetchData = async () => {
+    try {
+      const response = await axois.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=62a970fe8b3a0b1d02719076a3ebafab`
+      );
+      console.log(response);
+      setweatherData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleInputChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchData();
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter city name"
+          value={city}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Get weather</button>
+      </form>
+      {weatherData ? (
+        <>
+          <h2>{weatherData.name}</h2>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Description: {weatherData.weather[0].description}</p>
+          <p>Feels like: {weatherData.main.feels_like}°C</p>
+          <p>Humidity : {weatherData.main.humidity}%</p>
+          <p>Pressure : {weatherData.main.pressure}</p>
+          <p>Wind Speed : {weatherData.wind.speed}m/s</p> 
+        </>
+      ) : (
+        <p>Loading weather data.....</p>
+      )}
+    </div>
+  );
+};
+
+export default Weather;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  /* @__PURE__ */ jsxDEV(React.StrictMode, { children: /* @__PURE__ */ jsxDEV(App, {}, void 0, false, {
-    // ...
-  })}, void 0, false, {
-    // ...
-  })
-);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
